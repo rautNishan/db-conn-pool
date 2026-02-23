fd73b490260188a09e0e0d1bf6a60d97a15a6ed5
rautNishan <nishan.raut1709@gmail.com>
2026-02-23 20:48:20 +0545

    Implement diler

--- options.go
+++ options.go
@@ -1,12 +1,21 @@
 package dbconnpool
 
+import (
+	"context"
+	"net"
+	"time"
+)
+
 type Options struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	//Todo TSL CONFIG and timeouts (Dial timeout, read, write) and pools
+	Host        string
+	Port        string
+	User        string
+	Password    string
+	Database    string
+	Network     string
+	DialTimeout time.Duration
+	//Todo TSL CONFIG and timeouts (read, write) and pools
+	Diler func(ctx context.Context, netwrok string, addr string) (net.Conn, error)
 }
 
 func (opt *Options) init() {
@@ -17,4 +26,23 @@ func (opt *Options) init() {
 	if opt.Port == "" {
 		opt.Port = "5432"
 	}
+
+	if opt.Network == "" {
+		opt.Network = "tcp"
+	}
+
+	if opt.DialTimeout == 0 {
+		opt.DialTimeout = 5 * time.Second
+	}
+
+	if opt.Diler == nil { //Diler is a filed that can hold a function with that exact signature
+		opt.Diler = func(ctx context.Context, netwrok string, addr string) (net.Conn, error) {
+			netDialer := &net.Dialer{
+				Timeout:   opt.DialTimeout,
+				KeepAlive: 5 * time.Minute,
+			}
+			return netDialer.DialContext(ctx, netwrok, addr)
+		}
+	}
+
 }
