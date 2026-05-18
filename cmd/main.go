74e790d33f06f08c827ca6bdac40fa2263b26345
rautNishan <nishan.raut1709@gmail.com>
2026-05-18 21:31:52 +0545

    Remove everything

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,7 @@
 package main
 
-import dbconnpool "github.com/rautNishan/db-conn-pool"
+import "fmt"
 
 func main() {
-	db := dbconnpool.Connect(&dbconnpool.Options{
-		User:     "postgres",
-		Password: "root",
-		Database: "api-test",
-	})
-	db.Ping()
+	fmt.Println("This is test")
 }
--- pool/pool.go
+++
@@ -1,4 +0,0 @@
-package pool
-
-type Pooler interface {
-}
